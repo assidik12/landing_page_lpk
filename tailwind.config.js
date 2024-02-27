0e84c018adfac48f8df802eae3d7e32921ead520
maghfur_hasani <maghfurhasani9@gmail.com>
2024-02-27 16:02:42 +0700

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./**/*.{html, js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px"
+    },
     extend: {
+      margin: {
+        "mt-15": "60px"
+      },
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+        secondary: "#64748b"
+      },
       screens: {
+        "2xl": "1320px",
         xl: "1320px",
         sm: "450px",
       },
